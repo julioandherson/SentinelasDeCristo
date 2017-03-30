b50424ffcc4e38cf101350faab82e4b8af58ac24
diegotavarez <tavares.diego@outlook.com>
2017-03-29 22:07:39 -0300

    Tema alterado

--- site/assets/js/custom.js
+++ site/assets/js/custom.js
@@ -1,6 +1,6 @@
 /*=============================================================
     Authour URL: www.designbootstrap.com
-    
+
     http://www.designbootstrap.com/
 
     License: MIT
@@ -10,7 +10,7 @@
     100% Free To use For Personal And Commercial Use.
 
     IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
-   
+
 ========================================================  */
 
 $(document).ready(function () {
@@ -43,12 +43,11 @@ VAGAS SLIDESHOW SCRIPTS
 ======================================*/
 $.vegas('slideshow', {
 backgrounds: [
-{ src: 'assets/img/1.jpg', fade: 1000, delay: 9000 },
-{ src: 'assets/img/2.jpg', fade: 1000, delay: 9000 },
+{ src: 'assets/img/2.jpg', fade: 1000, delay: 9000},
 ]
 })('overlay', {
 /** SLIDESHOW OVERLAY IMAGE **/
-src: 'assets/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
+//src: 'assets/js/vegas/overlays/01.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
 });
 
 
